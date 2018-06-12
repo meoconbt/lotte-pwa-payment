0454400852b8d9423e1b6206a5edd791017ab912
Tran Le <lpbtran@tma.com.vn>
2018-06-12 15:59:54 +0700

    Update cors

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.set('view engine', 'jade');
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(session({
   // this string is not an appropriate value for a production environment
@@ -35,7 +37,7 @@ app.use(flash());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
 
   err.status = 404;
@@ -47,7 +49,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
+  app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -58,12 +60,25 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
+app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
+  next();
 });
 
 module.exports = app;
