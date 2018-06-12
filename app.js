592b893d32c7aa0c425c951737caff66425bb6b3
Tran Le <lpbtran@tma.com.vn>
2018-06-12 16:10:46 +0700

    Edit cors

--- app.js
+++ app.js
@@ -50,6 +50,18 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
