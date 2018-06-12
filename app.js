34ca0f06efa19a3430009c11df7fbd14345fb4fc
Tran Le <lpbtran@tma.com.vn>
2018-06-12 16:05:16 +0700

    Edit cors

--- app.js
+++ app.js
@@ -67,18 +67,18 @@ app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
   next();
+
+// res.status(err.status || 500);
+// res.render('error', {
+//   message: err.message,
+//   error: {}
+// });
 });
 
 module.exports = app;
